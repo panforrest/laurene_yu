83d1032429f81af395b57b270c8d5bfe1f8c223f
panforrest <guoqianp@gmail.com>
2018-03-24 16:11:08 -0400

    end v2 artTypes as dynamic data from backend to render on page

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,14 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-/*  This is the home route. It renders the index.mustache page from the views directory.
-	Data is rendered using the Mustache templating engine. For more
-	information, view here: https://mustache.github.io/#demo */
+const arts = [
+    {title:'Acting', img: 'LaureneYuActing.png', content: 'Acting and performing art redefine my brand.'},
+    {title:'Visual Art', img: 'laurene_design1.png', content: 'My design makes my customers enjoy life more.'},
+    {title:'Writing', img: 'writing.jpeg', content: 'My talents and passion for writing create what I really love'},
+]
+
 router.get('/', function(req, res){
-	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
+	res.render('index', {arts: arts})
 })
-
-/*  This route render json data */
-router.get('/json', function(req, res){
-	res.json({
-		confirmation: 'success',
-		app: process.env.TURBO_APP_ID,
-		data: 'this is a sample json route.'
-	})
-})
-
-/*  This route sends text back as plain text. */
-router.get('/send', function(req, res){
-	res.send('This is the Send Route')
-})
-
-/*  This route redirects requests to Turbo360. */
-router.get('/redirect', function(req, res){
-	res.redirect('https://www.turbo360.co/landing')
-})
-
 
 module.exports = router
